5b3f17f14e1f67797e6c608bee143f46488b11f4
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-08-23 22:27:02 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -60,6 +60,9 @@
         */
 
         redraw(){
+		var map = new BMapGL.Map("allmap");    // 创建Map实例
+		map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+		map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
         }
     });
 })();
