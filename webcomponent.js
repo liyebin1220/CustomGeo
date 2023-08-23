6c345f6e603827f69308f94eaf4ab7d2690ac470
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-08-23 22:07:38 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,17 +1,17 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World</h1>
+        <h1>Hello Geo Baidu</h1>
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-geobaidu01', class GeoBaidu01 extends HTMLElement {
 
 
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
+            		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            		this._firstConnection = false;
 		}
 
         //Fired when the widget is added to the html DOM of the page
