e17158b466929271f4d8eb7d3bea41fae4718ec0
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-08-29 01:59:40 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,10 +1,6 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		<style type="text/css">
 			#allmap {
    				background-color: #faebd7;
@@ -14,9 +10,6 @@
 	   		}
 		</style>
 		<script type="text/javascript" src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=eaRmogHU5j9QCWGS1KcLXnLnRIYF9Nyw"></script>
-		<title>地图展示</title>
-	</head>
-	<body>
 		<div id="allmap">all in The map</div>
   		<script type="text/javascript">
 		    // GL版命名空间为BMapGL
@@ -24,10 +17,7 @@
 			var map = new BMapGL.Map("allmap");    // 创建Map实例
 			map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
 			map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-		</script>
-	</body>
-	</html>
-	
+		</script>	
     `;
 
     customElements.define('com-sap-sample-geobaidu01', class GeoBaidu01 extends HTMLElement {
