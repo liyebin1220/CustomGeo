7e0ac5efdb17a9c88f9122ab683e3048e20aa70c
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-08-30 15:26:36 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -92,6 +92,11 @@
        		script.src = scriptSrc;
         	document.head.appendChild(script);
 
+		// GL版命名空间为BMapGL
+    		// 按住鼠标右键，修改倾斜角和角度
+		var map = new BMapGL.Map("allmap");    // 创建Map实例
+		map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+		map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 
         }
     });
