5c2ebbae9e53d16e960aa267dc210290b5ce49be
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-09-04 01:03:32 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -83,10 +83,8 @@
 	            mytable.appendChild(tr);
         	}
 		(function() {
-                    window.BMAP_PROTOCOL = "https"; 
-                    window.BMapGL_loadScriptTime = (new Date).getTime(); 
-                    document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?type=webgl&v=1.0&ak=eaRmogHU5j9QCWGS1KcLXnLnRIYF9Nyw&services=&t=20230817155609"></script>');
-                    document.write('<link rel="stylesheet" type="text/css" href="https://api.map.baidu.com/res/webgl/10/bmap.css"/>');
+                    document.write('<script type="text/javascript" src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=eaRmogHU5j9QCWGS1KcLXnLnRIYF9Nyw"></script>');
+                   
                 })();
             
             
