d1f9dd99ca7307bb2abc93dd3a0596a247057d4b
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-09-04 11:00:39 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -83,16 +83,16 @@
 	            mytable.appendChild(tr);
         	}
 		(function() {
-                    document.write('<script type="text/javascript" src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=eaRmogHU5j9QCWGS1KcLXnLnRIYF9Nyw"></script>');
+                    document.write('<script src="https://webapi.amap.com/loader.js"></script>');
                    
                 })();
             
             
             // GL版命名空间为BMapGL
             // 按住鼠标右键，修改倾斜角和角度
-            var map = new BMapGL.Map(allmap);    // 创建Map实例
-            map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-            map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+            //var map = new BMapGL.Map(allmap);    // 创建Map实例
+            //map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+            //map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 
         }
     });
