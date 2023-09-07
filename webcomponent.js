c345dd5b1725733835be189064e5d1c79ec90b6f
liyebin1220 <137027035+liyebin1220@users.noreply.github.com>
2023-09-07 23:26:26 +0800

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -8,6 +8,10 @@
     tmpl.innerHTML = `
 		<style>
           /* Add any custom CSS styles here */
+          #map-container {
+            width: 100%
+            height: 100%
+          }
         </style>
         <div id="map-container"></div>
     `;
